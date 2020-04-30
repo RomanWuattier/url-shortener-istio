993b6ce526e742e267125ce5874a0e10f0d75212
RomanWuattier <roman.wuattier@gmail.com>
2020-04-30 20:56:36 +0200

    Use the Jackson converter with Retrofit
    This commit removes the Gson converter to use Jackson instead.
    We should keep Jackson for consistency with Spring boot.

--- url-service/src/main/java/com/romanwuattier/urlservice/upstream/KeyGeneratorUpstreamService.java
+++ url-service/src/main/java/com/romanwuattier/urlservice/upstream/KeyGeneratorUpstreamService.java
@@ -1,14 +1,15 @@
 package com.romanwuattier.urlservice.upstream;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.Nullable;
 import retrofit2.Retrofit;
 import retrofit2.adapter.java8.Java8CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -23,14 +24,16 @@ public interface KeyGeneratorUpstreamService {
     @DELETE("key/release")
     CompletableFuture<ReleaseKeyResponse> releaseKey(@Query("key") String key);
 
-    @Data
-    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
     class KeyResponse {
         private String key;
     }
 
-    @Data
-    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
     class ReleaseKeyResponse {
         private boolean released;
         @Nullable
@@ -48,7 +51,7 @@ public interface KeyGeneratorUpstreamService {
             return new Retrofit.Builder()
                 .baseUrl("http://".concat(host).concat(":").concat(port))
                 .addCallAdapterFactory(Java8CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create())
                 .build()
                 .create(KeyGeneratorUpstreamService.class);
         }
